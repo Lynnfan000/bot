8b5efd17ff2e8d46371e79018a49e658fffc7cc0
Lynnfan000 <lynn.fan@adeptmind.ai>
2018-08-24 22:06:26 -0400

    web access

--- bot.py
+++ bot.py
@@ -3,14 +3,17 @@ from kik import KikApi, Configuration
 from kik.messages import messages_from_json, TextMessage
 import os
 
-
+print("start")
 app = Flask(__name__)
 kik = KikApi("chinyeebot", "8ed8ec43-b3c6-45d9-85e4-0e4442d592a4")
 
+print("will set config")
 kik.set_configuration(Configuration(webhook="https://chinyeebot.herokuapp.com/incoming"))
+
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
+    print("incoming")
     if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
         return Response(status=403)
 
@@ -27,8 +30,10 @@ def incoming():
             ])
 
     return Response(status=200)
+
+print("will run")
 # if __name__ == "__main__":
 app.run(port=int(os.environ['PORT']), debug=True)
-print(os.environ['PORT'])
+print("ran")
 
 
