5474d2aa4f485793d1585d474606c425eff66434
Lynnfan000 <lynn.fan@adeptmind.ai>
2018-08-24 20:47:14 -0400

    example

--- bot.py
+++ bot.py
@@ -1,6 +1,31 @@
-def a (x):
-    x = 10
-    if x==10:
-        print("hello world")
-    else:
-        print("lol")
+from kik import KikApi
+from flask import Flask
+from kik.messages import messages_from_json, TextMessage
+
+app = Flask(__name__)
+kik = KikApi("chinyeebot", "8ed8ec43-b3c6-45d9-85e4-0e4442d592a4")
+
+kik.set_configuration(Configuration(webhook="https://chinyeebot.herokuapp.com/incoming"))
+
+@app.route('/incoming', methods=['POST'])
+def incoming():
+    if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
+        return Response(status=403)
+
+    messages = messages_from_json(request.json['messages'])
+
+    for message in messages:
+        if isinstance(message, TextMessage):
+            kik.send_messages([
+                TextMessage(
+                    to=message.from_user,
+                    chat_id=message.chat_id,
+                    body=message.body
+                )
+            ])
+
+    return Response(status=200)
+
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
