bfdd0e14db7cf8adc5c4ff63c9d347b36a8dd524
Lynnfan000 <lynn.fan@adeptmind.ai>
2018-08-26 19:49:27 -0400

    update script

--- bot.py
+++ bot.py
@@ -3,22 +3,18 @@ from kik import KikApi, Configuration
 from kik.messages import messages_from_json, TextMessage
 import os
 
-print("start")
 app = Flask(__name__)
 kik = KikApi("chinyeebot", "8ed8ec43-b3c6-45d9-85e4-0e4442d592a4")
 
-print("will set config")
 kik.set_configuration(Configuration(webhook="https://chinyeebot.herokuapp.com/incoming"))
 
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
-    print("incoming")
     if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
         return Response(status=403)
 
     messages = messages_from_json(request.json['messages'])
-
     for message in messages:
         if isinstance(message, TextMessage):
             kik.send_messages([
@@ -28,13 +24,11 @@ def incoming():
                     body=message.body
                 )
             ])
-
+            print(message.body)
     return Response(status=200)
 
-print("will run")
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-print("ran")
 
 
