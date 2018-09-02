665c20151f30373e26066b2d29617733a3290fc4
Lynnfan000 <lynn.fan@adeptmind.ai>
2018-09-02 17:50:29 -0400

    connect to db

--- bot.py
+++ bot.py
@@ -2,13 +2,22 @@ from flask import Flask, request, Response
 from kik import KikApi, Configuration
 from kik.messages import messages_from_json, TextMessage
 import os
+import sys
+import urlparse
+import mysql.connector
+
 
 app = Flask(__name__)
 kik = KikApi("chinyeebot", "8ed8ec43-b3c6-45d9-85e4-0e4442d592a4")
+mydb = mysql.connector.connect(
+  host="us-cdbr-iron-east-01.cleardb.net",
+  user="b68cd089abc81d",
+  passwd="2c2f1c38",
+  database="heroku_5a951cfac26923b"
+)
+
 
 kik.set_configuration(Configuration(webhook="https://chinyeebot.herokuapp.com/incoming"))
-
-#("Hi, I'm your rating lunch bot, if you wanna rate your lunch, type\"rate lunch\", if you wanna see the menu, type\"see menu\"")
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
@@ -41,7 +50,7 @@ def incoming():
                 TextMessage(
                     to=message.from_user,
                     chat_id=message.chat_id,
-                    body="I don't understand, please try again")])# need to reset the conversation and start over
+                    body="I don't understand, you can type\"rate lunch\" to rate your lunch or \"see menu\" to see the menu")])
         return Response(status=200)
 
 if __name__ == "__main__":
