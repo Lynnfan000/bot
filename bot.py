d1b83ce71507c85b26df83aac87272a6a6aaa577
Lynnfan000 <lynn.fan@adeptmind.ai>
2018-09-05 13:51:02 -0400

    update

--- bot.py
+++ bot.py
@@ -5,6 +5,14 @@ import os
 import sys
 import mysql.connector
 
+state=0
+
+class mymenu:
+    def __init__(self, date, menu):
+        self.date = date
+        self.menu = menu
+my_date=datetime.datetime.today().strftime('%Y-%m-%d')
+
 
 app = Flask(__name__)
 kik = KikApi("chinyeebot", "8ed8ec43-b3c6-45d9-85e4-0e4442d592a4")
@@ -15,21 +23,21 @@ mydb = mysql.connector.connect(
   database="heroku_5a951cfac26923b"
 )
 mycursor = mydb.cursor()
-kik.set_configuration(Configuration(webhook="https://chinyeebot.herokuapp.com/incoming"))
-
-@app.route('/incoming', methods=['POST'])
-state = 0
-my_date = datetime.datetime.today().strftime('%Y-%m-%d')
-
 mycursor.execute("SELECT menu FROM lunch_info WHERE mydate = my_date")
-my_menu = mycursor.fetchall()
+my_menu=mycursor.fetchall()
+
+today = mymenu(my_date, my_menu)
+
+kik.set_configuration(Configuration(webhook="https://chinyeebot.herokuapp.com/incoming"))
+@app.route('/incoming', methods=['POST'])
+
 def incoming():
     if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
         return Response(status=403)
     messages = messages_from_json(request.json['messages'])
     for message in messages:
         if isinstance(message, TextMessage):
-            if state = 1: #this is a comment from user      
+            if state==1: #this is a comment from user      
                 sql = "INSERT INTO comment (date, menu, name, comment) VALUES (%s, %s, %s, %s)"
                 val = (my_date, my_menu, message.from_user, message.body)
                 mycursor.execute(sql, val)
@@ -39,35 +47,35 @@ def incoming():
                         to=message.from_user,
                         chat_id=message.chat_id,
                         body="Okay, got that!")])
-                state = 0
+                state=0
             elif message.body.lower() in ["hi", "hello", "hi!", "hello!", "hey"]:
                 kik.send_messages([
                     TextMessage(
                         to=message.from_user,
                         chat_id=message.chat_id,
                         body="Hi, I'm your rating lunch bot, if you wanna rate your lunch, type\"rate lunch\", if you wanna see the menu, type\"see menu\"")])
-                state = 0
+                state=0
             elif message.body.lower() == "rate lunch":
                 kik.send_messages([
                     TextMessage(
                         to=message.from_user,
                         chat_id=message.chat_id,
                         body="Here is your lunch menu: "+my_menu)])
-                state = 1
+                state=1
             elif message.body.lower() == "see menu":
             	kik.send_messages([
                     TextMessage(
                         to=message.from_user,
                         chat_id=message.chat_id,
                         body="Here is your lunch menu: "+my_menu)])#need to communicate with database
-                state = 0
+  #              state=0
             else:
                 kik.send_messages([
                 TextMessage(
                     to=message.from_user,
                     chat_id=message.chat_id,
                     body="I don't understand, you can type\"rate lunch\" to rate your lunch or \"see menu\" to see the menu")])
-                state = 0
+                state=0
         return Response(status=200)
 
 if __name__ == "__main__":
